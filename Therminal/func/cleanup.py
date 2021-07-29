694dc454b47b0bb6a8d727a13e2f1e37b37e7a5a
2b2b2b <59747023+Scotchex@users.noreply.github.com>
2021-07-30 02:00:10 +0300

    Add files via upload

---
+++ Therminal/func/cleanup.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from termcolor import colored, cprint
+from pyfiglet import Figlet
+from requests import get
+from os import system, name
+from time import sleep
+
+def cliCleanup():
+    _ = system('clear')
