f0c8419136efe6f2e0a5737361e9db22f50d9ce2
2b2b2b <59747023+Scotchex@users.noreply.github.com>
2021-07-30 01:56:40 +0300

    Beta Version
    Just check the comments in the code for more info regarding the code, and also I am open for quesitons.

---
+++ Therminal/build/options.py
@@ -0,0 +1,24 @@
+#this is to be filled with a list of stuff this#
+#terminal is capable of (turning on lights etc)#
+#for now I am just gonna throw in some place holders#
+
+import os
+import sys
+from termcolor import colored, cprint
+from pyfiglet import Figlet
+from requests import get
+from os import system, name
+from time import sleep
+
+
+
+def showOptions():
+    print(colored(
+"""
+0. Clean the terminal
+1. Learn your IP adress    
+2. Print 'I love cats'    
+
+"""
+, 'yellow'))
+
