a32b1efa4ec987e1e1dcaf52ed2527b70a9500a5
2b2b2b <59747023+Scotchex@users.noreply.github.com>
2021-08-04 09:58:24 +0300

    Add files via upload

---
+++ build/python_idle.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from os import system, name
+from termcolor import colored, cprint
+from pyfiglet import Figlet
+from build.welcome import welcome
+from build.options import showOptions
+
+def launchCLI(arg):
+    _ = system('clear')
+    _ = system('cls')
+    f = Figlet(font = 'banner')
+    print(colored(f.renderText('Therminal\n IDLE\n\n'), 'red'))
+    while True:
+        a = str(input('>> '))
+        if a == 'exit':
+            quit()
+        elif a == '0':
+            _ = system('clear')
+            _ = system('cls')
+            welcome()
+            showOptions()
+            break
+        else:
+            exec(a)
+            
